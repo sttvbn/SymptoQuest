ff2ac23ba31d395c35255027c22831f1675e1662
Douglasmyu <123704099+Douglasmyu@users.noreply.github.com>
2024-07-15 13:27:17 -0700

    Update +page.svelte

--- src/routes/GPT/+page.svelte
+++ src/routes/GPT/+page.svelte
@@ -1,52 +1,72 @@
-<script>
-    let prompt = "";
-    let messages = [];
-
-    async function sendPrompt(){
-        const response = await fetch("/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded", 
-            },
-            body: 'prompt=${encodeURIComponent(prompt)}'
-        });
-        const message = await response.json();
-        messages = [...messages, message.data];
-    }
-</script>
+<h1>SymptoQuest chat</h1>
+<h2>lets chat</h2>
 <style>
-    h1 {
+    h1{
         font-size: 36px;
-        font-family: Arial, sans-serif; 
-        text-align: center; 
+        text-align: center;
     }
-
     form {
         display: flex;
         flex-direction: column;
         align-items: center;
     }
-
-    textarea {
-        width: 300px; 
+    textarea{
+        width: 300px;
         margin-bottom: 10px;
+        align-items: center;
     }
-
-    button {
+    button{
         width: 100px;
         padding: 5px;
+        color:blue
+    }
+    label{
+        align-items: center;
+    }
+    nav a{
+        float: left;
+        display: block;
+        color: rgb(198, 23, 23);
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none; 
+    }
+    nav a:hover{
+        background-color: #b70d0d;
+        color: black;
     }
 </style>
-<h1>SvelteGPT</h1>
+
+<script lang="ts">
+    let prompt:string = "";
+    let messages:string[] = [];
+
+    async function sendPrompt() {
+        const response = await fetch("/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `prompt=${encodeURIComponent(prompt)}`
+        });
+        const message = await response.json();
+        messages = [...messages, message.data];
+    }
+</script>
+<nav>
+    <a href="home">Home</a>
+    <a href="about">About</a>
+    <a href="Maps">Maps</a>
+</nav>
 
 <form method="POST" on:submit|preventDefault={sendPrompt}>
     <label for="prompt">
         Prompt:
-        <textarea name="prompt" rows="4"  bind:value={prompt}></textarea>
+        <textarea name="prompt" rows="5" bind:value={prompt}></textarea>
+        <button type="submit">Send</button>
     </label>
-    <button type="submit">Send</button>
 </form>
 
-{#each messages as message }
+{#each messages as message}
     <pre>{message}</pre>
-{/each}
+    {/each}
