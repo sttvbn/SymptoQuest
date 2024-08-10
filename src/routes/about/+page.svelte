2c332d7e763b1f1accce98824c691ecdf10129b4
sttvbn <112314040+sttvbn@users.noreply.github.com>
2024-08-10 10:48:33 -0700

    updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -28,10 +28,10 @@
 
     <div class="content">
         <h1>WELCOME TO SYMPTOQUEST!</h1>
-        <p>Our mission is to offer a symptom checker that's designed for users to utilize for medical needs with the use of AI technology. 
-        SymptoQuest aims to help users identify potential medical conditions, but please be aware that the purpose of SymptoQuest is designed 
-        for educational purposes only and shouldn't be considered a substitute for professional medical advice. For any real medical emergencies or concerns, 
-        please consult with a qualified healthcare professional. Thank you.</p>
+        <p>Our mission is to offer a symptom checker designed for users to utilize for medical needs with AI technology. 
+        SymptoQuest aims to help users identify potential medical conditions. Still, please be aware that the purpose of 
+        SymptoQuest is designed for educational purposes only and shouldn't be considered a substitute for professional medical advice. 
+        Please consult with a qualified healthcare professional for any real medical emergencies or concerns. Thank you.</p>
     </div>
    
 
@@ -55,6 +55,14 @@
     border-radius: 8px;
     font-weight: bold;
     
+}
+
+h1{
+    font-size: 50px;
+}
+
+p{
+    font-size: 25px;
 }
 
 ul {
